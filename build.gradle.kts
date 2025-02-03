e29dc2a698991e0fea2823a80ad125e50fa16d9f
leandrodalbo <leandro.edbo@gmail.com>
2025-02-03 00:02:49 +0000

    testing publishing to maven central workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.vanniktech.maven.publish") version "0.30.0"
-    //id("maven-publish")
 }
 
 java {
@@ -15,7 +14,7 @@ java {
 
 repositories {
     mavenCentral()
-    gradlePluginPortal()
+    mavenLocal()
 }
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
@@ -49,6 +48,41 @@ tasks.named<Jar>("jar") {
 tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
     enabled = false
 }
+
+mavenPublishing {
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL, true)
+    if (!project.gradle.startParameter.taskNames.any { it.contains("publishToMavenLocal") }) {
+        signAllPublications()
+    }
+
+    coordinates("io.bootsolana", "solana-boot-starter",  "1.0.0")
+
+    pom {
+        name = "Solana Boot Starter"
+        description = "Integrate Spring Boot with Solana"
+        inceptionYear = "2025"
+        url = "https://bootsolana.io"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/licenses/MIT"
+            }
+        }
+        developers {
+            developer {
+                id = "bootsolana"
+                name = "bootsolana"
+                url = "https://github.com/bootsolana"
+            }
+        }
+        scm {
+            url = "https://github.com/bootsolana/solana-boot-starter"
+            connection = "scm:git:git://github.com:bootsolana/solana-boot-starter.git"
+            developerConnection = "scm:git:ssh://git@github.com:bootsolana/solana-boot-starter.git"
+        }
+    }
+}
+
 
 
 //publishing {
